96185c0369dc76068cf03e2517091e67ac6fa53a
unknown <Hubber@.(none)>
2014-07-21 18:49:22 -0500

    clase Jugador

--- juego/jugador.js
+++ juego/jugador.js
@@ -2,3 +2,14 @@
 //Definimos la clase Jugador que se extiende la clase Sprite
 //Sprite pertenece al core de Quintu
 //nombre de la clase y objeto de configuración de la clase
+
+Q.Sprite.extend("Jugador",{
+	init: function(p){
+		this._super(p,{
+			sheet: "jugador",
+			frame: 1,
+			jumpSpeed: -380,
+			speed:120
+		});
+	}
+});
