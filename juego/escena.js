3340afb0722f882c71e93daab297586926b707f0
unknown <Hubber@.(none)>
2014-07-21 18:57:43 -0500

    stage tmx

--- juego/escena.js
+++ juego/escena.js
@@ -1,3 +1,9 @@
 //D63
 //indicar el nombre de la escena y el callback
 //se carga el mapa
+
+Q.scene("mundo1", function(stage){
+	//confugurar escena (stage)
+	//cargar el archivo TMX
+	Q.stageTMX("mundo1_terminado.tmx",stage);
+} );
