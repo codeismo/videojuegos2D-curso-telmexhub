cf110da0df6fc7f9d294796028fbffb64ad9a814
unknown <Hubber@.(none)>
2014-07-21 18:29:02 -0500

    inicializar

--- juego/inicializar.js
+++ juego/inicializar.js
@@ -1,8 +1,16 @@
 //accedemos a las funcionalidades del engine
+var Q = Quintus();
 
 //el juego se ejecutará en la etiqueta del canvas cuyo id es juego
 //opcionalmente se puede dar como segundo parametro un objeto de configuración
+Q.setup("juego", {
+	maximize: true
+});
 
 //modulos a utilizar (6)
+Q.include("Sprites, Scenes, 2D, Input, Touch, TMX");
 
 //activamos los controles del teclado y controles touch
+Q.controls();
+Q.touch();
+//Q.controls().touch();
