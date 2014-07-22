1191e99607478419e38a26055254d81df7abbd9f
unknown <Hubber@.(none)>
2014-07-21 19:08:03 -0500

    cargar recursos

--- juego/cargar_recursos.js
+++ juego/cargar_recursos.js
@@ -1,3 +1,12 @@
 //D66
 //indicar en una cadena los recursos: audios, imagenes, tmx, json
 //callback para configurar los sprites
+var recursos = "jugador.json, mundo1_terminado.tmx, mosaicos_escenario_32x32.png, mosaicos_mario_enano_30x30.png";
+
+Q.loadTMX(recursos, function(){
+	//se ejecuta hasta que los recursos estén listos
+	//compilar el spritesheet del jugador
+	Q.compileSheets("mosaicos_mario_enano_30x30.png", "jugador.json");
+	//ejecutamos la escena
+	Q.stageScene("mundo1");
+});
