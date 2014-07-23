962d36ac650c2e15769042678ba9eedb7a632881
unknown <Hubber@.(none)>
2014-07-23 16:53:24 -0500

    x, y: viewport

--- juego/escena.js
+++ juego/escena.js
@@ -6,8 +6,21 @@ Q.scene("mundo1", function(stage){
 	//confugurar escena (stage)
 	//cargar el archivo TMX
 	Q.stageTMX("mundo1_terminado.tmx",stage);
+	
+	//obtener mi capa de fondo
+	var capaFondo = Q("TileLayer").first();
 
 	//La camara siga a Mario
-	stage.add("viewport").follow( Q("Jugador").first() );
+	stage.add("viewport").follow( Q("Jugador").first(),{
+		x: true,
+		y: true
+	},{
+		minX:32,
+		//ancho de la capa de cielo 
+		//es el mismo que el ancho del canvas
+		maxX: capaFondo.p.w - 32,
+		minY: 0,
+		maxY: capaFondo.p.h
+	} );
 
 } );
