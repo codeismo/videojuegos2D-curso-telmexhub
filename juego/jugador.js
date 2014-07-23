27bef7af326a59536dfcb73115b851e41164c4b3
unknown <Hubber@.(none)>
2014-07-23 16:34:08 -0500

    girando al jugador

--- juego/jugador.js
+++ juego/jugador.js
@@ -8,9 +8,20 @@ Q.Sprite.extend("Jugador",{
 		this._super(p,{
 			sheet: "jugador",
 			frame: 1,
-			jumpSpeed: -380,
-			speed:120
+			jumpSpeed: -800,
+			speed:150
 		});
 		this.add("2d, platformerControls");
+	},
+	//esta funcion se repite continuamente (Game Loop)
+	step: function(){
+		//Si el jugador va a la izquierda y tecleo derecha
+		if(this.p.direction === "left" && Q.inputs["right"]){
+			this.p.flip = false;
+		}
+		//si el jugador va a la derecha y tecleo izquierda
+		if(this.p.direction == "right" && Q.inputs["left"]){
+			this.p.flip= "x";
+		}
 	}
 });
