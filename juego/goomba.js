9e45e67b549b6c83116bac931ccfbbd3df82dd49
unknown <Hubber@.(none)>
2014-07-23 19:02:34 -0500

    goomba muere trigger

--- juego/goomba.js
+++ juego/goomba.js
@@ -1,41 +1,42 @@
 //Grupo de animaciones de Goomba
-Q.animations("animacionesGoomba",{
-	caminar:{
-		frames:[0,1],
-		rate: 1 / 2,
-		loop: true
+Q.animations("animacionesGoomba", {
+	caminar : {
+		frames : [0, 1],
+		rate : 1 / 2,
+		loop : true
 	},
-	aplastar:{
-		frames: [3],
-		rate: 1 / 2,
-		loop: false,
-		trigger: "destruir"
+	aplastar : {
+		frames : [3],
+		rate : 1 / 2,
+		loop : false,
+		trigger : "destruir"
 	}
 });
 
-Q.Sprite.extend("Goomba",{
-	init: function(p){
-		this._super(p,{
-			sprite: "animacionesGoomba",
-			sheet: "goomba",
-			frame: 0,
-			vx:170
+Q.Sprite.extend("Goomba", {
+	init : function(p) {
+		this._super(p, {
+			sprite : "animacionesGoomba",
+			sheet : "goomba",
+			frame : 0,
+			vx : 170
 		});
 		this.add("2d, aiBounce, animation");
 		//Ejecuta la animacion caminar siempre
 		this.play("caminar");
 		//escuchar las colisiones por arriba
 		this.on("bump.top", this, "aplasta");
+		//trigger
+		this.on("destruir", function() {
+			this.destroy();
+		});
 	},
-	aplasta: function(colision){
-			//revisar si colisione con Mario
-			if( colision.obj.isA("Jugador") ){
-				//Goomba muere
-				this.play("aplastar");
-				//this.destroy();
-			}
-	},
-	destruir: function(){
-		this.destroy();
+	aplasta : function(colision) {
+		//revisar si colisione con Mario
+		if (colision.obj.isA("Jugador")) {
+			//Goomba muere
+			this.play("aplastar");
+			//this.destroy();
+		}
 	}
-});
+}); 
