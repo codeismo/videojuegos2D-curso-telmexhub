6f117f8d43eaaa04dc7e072526b9e252c7f0eca4
unknown <Hubber@.(none)>
2014-07-24 17:08:53 -0500

    audio

--- juego/inicializar.js
+++ juego/inicializar.js
@@ -1,5 +1,7 @@
 //accedemos a las funcionalidades del engine
-var Q = Quintus();
+var Q = Quintus({
+	audioSupport: ["mp3", "ogg"]
+});
 
 //el juego se ejecutará en la etiqueta del canvas cuyo id es juego
 //opcionalmente se puede dar como segundo parametro un objeto de configuración
@@ -8,7 +10,10 @@ Q.setup("juego", {
 });
 
 //modulos a utilizar (6)
-Q.include("Sprites, Scenes, 2D, Input, Touch, TMX, Anim");
+Q.include("Sprites, Scenes, 2D, Input, Touch, TMX, Anim, Audio");
+
+//activamos el sonido
+Q.enableSound();
 
 //activamos los controles del teclado y controles touch
 Q.controls();
